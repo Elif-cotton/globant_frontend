0844d52c8c07164ed952fe59d518e3a0e0219a9f
“elif-cotton” <elif.watercolors@gmail.com>
2024-02-27 19:40:31 -0300

    cambiar imagen miniatura

--- React/proyecto1-app/virtual-store/src/components/Thumbs.tsx
+++ React/proyecto1-app/virtual-store/src/components/Thumbs.tsx
@@ -1,20 +1,37 @@
-import  "../components/Thumbs.module.css";
+import styles from "../components/Thumbs.module.css";
+import { useState } from 'react'; // Importa el hook useState
 
-function Thumbs({ product }) {
+interface Product {
+  images: string[];
+}
+
+interface Props {
+  product: Product;
+}
+
+function Thumbs({ product }: Props) {
+  const [thumb, setThumb] = useState<string>(product.images[0]);
+
     return <>
-         <div>
-            <div className="product-images-block">
-              <div className="thumbnail-container">
-                <img className="thumbnail" src={product.images[1]} alt="" />
-              </div>
-              <div className="thumbnail-container">
-                <img className="thumbnail" src={product.images[2]} alt="" />
-              </div>
+          <div>
+                <div className="product-images-block">
+                    {/* Mapea sobre las miniaturas y configura el evento onClick para cambiar el estado thumb */}
+                    {product.images.map((image, index) => (
+                        <div key={index} className="thumbnail-container">
+                            <img
+                                className={styles.thumbnail}
+                                src={image}
+                                alt={image}
+                                onClick={() => setThumb(image)}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-          <div className="thumbnail-container">
-            <img id="selected-thumbnail" src={product.images[0]} alt="" />
-          </div>
+            <div className="thumbnail-container">
+                {/* Configura el atributo src de la imagen principal para mostrar la imagen almacenada en el estado thumb */}
+                <img id="selected-thumbnail" src={thumb} alt="" />
+            </div>
     </>
 }
 
